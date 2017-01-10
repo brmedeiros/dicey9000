07f3b293be6e6d6ff6100fb62ee2a835010585ce
brmedeiros <br.medeiros@gmail.com>
2017-01-10 16:51:23 -0300

    creates the file new_dicey9000.py to that implements the changes in new_dice.py
    *updates Procfile so it runs new_dicey9000.py instead of
    dicey9000.py
    
    *updates to version 2.0
    
    *updates README.md and readme.txt

---
+++ new_dicey9000.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import os
+import discord
+import asyncio
+import random
+import datetime as dt
+import new_dice
+import dice_config as dcfg
+import dice_exceptions as dexc
+import aux_functions as auxf
+
+def main():
+
+    client = discord.Client()
+
+    @client.async_event # equivalent to @client.event\n@asyncio.courotine
+    def on_ready():
+        '''
+        this function represents the event 'being ready'...
+        it should not be called anywhere and it is defined in the discord module
+        '''
+        print('DICEY9000 v.2.0\n------\nLogged in as {0}, id: {1}'.format(client.user, client.user.id))
+        auxf.sp_print('Server(s) joined:')
+        for server in client.servers:
+            auxf.sp_print('{0}'.format(server))
+        print('\n------')
+
+    @client.async_event
+    def on_message(message):
+        '''
+        this function represents a 'message event' in any of the channels...
+        it should not be called anywhere and it is defined in the discord module
+        '''
+        if message.author == client.user:
+            print('{:%d/%m/%y %H:%M} @{} {}: {}'
+                  .format(dt.datetime.now(), message.channel, message.author.name, message.content))
+
+        if message.content.startswith('!r'):
+            print('{:%d/%m/%y %H:%M} @{} user: {}'
+                  .format(dt.datetime.now(), message.channel, message.content))
+            try:
+                will_roll = True
+                number_of_dice, dice_type, modifier, explode, success, dcfg.mode, cmd_msg\
+                = new_dice.dice_input_verification(message.content, dcfg.mode)
+
+                if cmd_msg != None:
+                    yield from client.send_message(message.channel, cmd_msg)
+                    will_roll = False
+
+            except (dexc.SuccessConditionError, dexc.ExplodingDiceError,
+                    dexc.ExplodingDiceTooSmallError, dexc.RollInputError) as ex:
+                yield from client.send_message(message.channel, dexc.dice_exception_msg(ex, ex.msg))
+                will_roll = False
+
+            if will_roll == True:
+                my_roll = new_dice.DiceRoll(number_of_dice, dice_type, modifier, explode, success)
+                my_roll.roll_dice()
+                my_roll.explode_dice()
+                my_roll.success_counter()
+                yield from client.send_message(message.channel, my_roll.output())
+
+                # formated_results, success_msg = dice.dice_roll(number_of_dice, dice_type, explode, success)
+                # results_string = '  '.join(formated_results)
+                # yield from client.send_message(message.channel, results_string)
+                # if success_msg != None:
+                #     yield from client.send_message(message.channel, success_msg)
+
+    token = os.environ['DICEY9000_TOKEN']
+    client.run(token)
+
+if __name__ == '__main__':
+    main()
